892e91756d6b2450f1e0131f4bf9d5ec7b944e3a
Jakub Klimkowski <sadgi.enjoyer@protonmail.com>
2025-04-17 12:45:17 +0200

    change navbar to sidebar

---
+++ src/renderer/src/components/sidebar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Settings } from 'lucide-svelte';
+    import namilogo from '../../../../resources/namilogo.svg';
+</script>
+
+<div class="h-screen w-[8vw] bg-primary text-white flex flex-col">
+    <div class="p-4 flex flex-col items-center justify-center">
+        <img src={namilogo || "/placeholder.svg"} alt="Logo" class="w-[6vw] h-[6vw]" />
+    </div>
+    
+    <div class="flex-grow"></div>
+    
+    <div class="p-4 flex justify-center mb-2">
+        <button class="p-2 rounded-lg hover:bg-secondary transition flex items-center justify-center">
+            <Settings class="w-[3vw] h-[3vw]" />
+        </button>
+    </div>
+</div>
